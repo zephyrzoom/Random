e5fa9bd751d7b2de8f298c3e106061a6b34a11ec
707 <707472783@qq.com>
2016-06-25 03:44:45 +0800

    add electron version

---
+++ ele/index.js
@@ -0,0 +1,32 @@
+const electron = require('electron');
+const {app} = electron;
+const {BrowserWindow} = electron;
+
+let win;
+
+function createWindow() {
+  // Create the browser window.
+  win = new BrowserWindow({width: 600, height: 400, autoHideMenuBar: true});
+
+  // and load the index.html of the app.
+  win.loadURL(`file://${__dirname}/index.html`);
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
